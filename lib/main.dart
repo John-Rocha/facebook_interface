e11aa90a03951855ea2a2bd4390f224512407397
Johnathan Rocha <johnathanrocha@gmail.com>
2021-09-05 12:39:18 -0300

    Sliver AppBar done

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:facebook_interface/telas/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Facebook",
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
---
+++ lib/utils/paletas_cores.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+class PaletasCores {
+  static const Color azulFacebook = Color(0xff1777f2);
+}
